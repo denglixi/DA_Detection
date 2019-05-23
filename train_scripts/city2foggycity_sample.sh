63b28ac7e6d8b2492d1f545fdd18ac4a968f845f
denglixi <denglixi1991@gmail.com>
2019-05-23 10:56:00 +0800

    train city2foggy. modify the args.imdb during training

--- train_scripts/city2foggycity_sample.sh
+++ train_scripts/city2foggycity_sample.sh
@@ -3,4 +3,4 @@ CUDA_VISIBLE_DEVICES=$1 python ./app/trainval_net_global_local.py \
     --cuda --net vgg16 \
     --dataset cityscape --dataset_t foggy_cityscape \
     --gc --lc --save_dir models \
-    --nw 5
+    #--nw 4
