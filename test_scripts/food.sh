177b2c7d375cd4dce668557f92e012df72fea880
denglixi <denglixi1991@gmail.com>
2019-08-07 20:34:22 +0800

    add nocross training&test

--- test_scripts/food.sh
+++ test_scripts/food.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # test collected canteen
-#DATASET=foodTechChickenmt10
+DATASET=foodSciencemt10
 
 # test excl canteen
-DATASET=foodexclTechChicken
+# DATASET=foodexclScience
 
-CUDA_VISIBLE_DEVICES=$1 python ./app/test_net_global_local.py --cuda --net prefood --dataset $DATASET --gc --lc --load_name $2 \
+CUDA_VISIBLE_DEVICES=$1 python ./app/test_net_global_local.py --cuda --net prefood --dataset $DATASET --load_name $2 \
     --nw 10 \
+    --lc --gc
     #--test_cache
+    #--gc --lc  \
--- train_scripts/food.sh
+++ train_scripts/food.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 NW=0
 #{ foodexclArtsmt10 }
-DATASET=foodexclTechChickenmt10
-DATASET_T=foodTechChicken
-CUDA_VISIBLE_DEVICES=$1 python ./app/trainval_net_local.py --cuda --net prefood \
+DATASET=foodexclSciencemt10
+DATASET_T=foodScience
+
+#trainval_net_no_cross
+#trainval_net_global_local
+
+
+
+# !! train_domain_loss is necessary for training the domain align
+
+CUDA_VISIBLE_DEVICES=$1 python ./app/trainval_net_no_cross.py --cuda --net prefood \
     --dataset $DATASET --dataset_t $DATASET_T \
     --gc --lc --save_dir $2 \
-    --nw $NW
+    --nw $NW \
+    --fixed_layer=4 \
+    --train_domain_loss
